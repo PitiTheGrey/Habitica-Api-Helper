97a2c17fcfe5482b41a1f57bee3ba7435988471c
PitiTheGrey <35431804+PitiTheGrey@users.noreply.github.com>
2020-07-12 21:46:12 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,9 @@
-let ID = ["xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx", "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyy"];
-let token = ["xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx", "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyy"];
+// multiple UUIDs
+let ID = ["xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"];
+let token = ["xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"];
 let index = 0; 
+// access paths for either internet or local install of habitica
+let internet = "https://habitica.com/api/v3/";
+let localhost ="http://localhost:3000/api/v3/"; // standard path for habitica set up locally
+// webhook URL
+let webhookUrl = "https://script.google.com/macros/s/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxP/exec";
